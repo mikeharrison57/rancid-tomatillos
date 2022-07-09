f4cde21d395781dd70e853fcf1c1307acd478ebe
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-07-08 19:53:36 -0600

    feat: import movie data to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import React, { Component } from 'react';
+import movieData from './mock-movie-data'
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      movies: [movieData]
+    }
   }
 
   render() {
+    console.log(this.state.movies)
     return (
       <h2>Michael</h2>
     )
