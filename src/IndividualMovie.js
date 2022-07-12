e3d24948c7bb7df22742b40837192d5d65b97ce5
Nathan Hodnett <nathanhodnett@gmail.com>
2022-07-11 20:55:56 -0600

    Add css to Individual Movie view

--- src/IndividualMovie.js
+++ src/IndividualMovie.js
@@ -6,18 +6,22 @@ const IndividualMovie = (props) => {
         id, title, poster_path, backdrop_path, release_date, overview, average_rating, genres, budget, revenue, runtime, tagline 
     } = props.movie
 
+    const splitDate = release_date.split("-").shift()
+    const roundedRating = average_rating.toFixed(2)
+    const splitGenres = genres.join(", ")
+
     return(
         <div className='IndividualMovie'>
             {/* <p>{id}</p> */}
-            <p className='Title'>{title}</p>
             {/* <img src={poster_path}></img> */}
             <div className='BackgroundImage'>
-                <img src={backdrop_path}></img>
+            <img src={backdrop_path}></img>
             </div>
-            <p>Release Date: {release_date}</p>
+            <p className='Title'>{title}</p>
+            <p>Release Date: {splitDate}</p>
             <p>Overview: {overview}</p>
-            <p>Rating: {average_rating}</p>
-            <p className='Genres'>Genres: {genres}</p>
+            <p>Rating: {roundedRating}</p>
+            <p className='Genres'>Genres: {splitGenres}</p>
             {/* <p>{budget}</p>
             <p>{revenue}</p> */}
             <p>Runtime: {runtime}</p>
